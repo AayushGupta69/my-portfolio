5f5f6b5b21a07928a0aaf3878e58b683e770449b
Aayush Gupta <72346589+AayushGupta69@users.noreply.github.com>
2024-09-12 16:34:38 +0530

    Added Work Experience section (#9)
    * Added svgs for experience
    
    * Added new Aceternity UI Moving Borders component
    
    * Completed experience section
    
    * Added id for experience
    
    * Added experience section in page.tsx
    
    * Made the page.tsx a client component.
    
    * Added work experience data
    
    * Changed div to section
    
    * Fixed typo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 const Footer = dynamic(() => import("@/components/Footer"));
+const Experience = dynamic(() => import("@/components/Experience"));
 import Hero from "@/components/Hero";
 const RecentProjects = dynamic(() => import("@/components/RecentProjects"), {
   ssr: false,
@@ -14,6 +17,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
+        <Experience />
         <RecentProjects />
         <Testimonials />
         <Footer />
