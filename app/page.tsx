5bfd98db37d30c43d01a71ca7e4489459dab3fad
Aayush Gupta <72346589+AayushGupta69@users.noreply.github.com>
2024-09-12 12:17:00 +0530

    Fixing bugs and added dynamic import for Footer section (#6)
    * Fixed typo
    
    * Fixed overflow issue with navbar
    
    * Used dynamic import for Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer";
+const Footer = dynamic(() => import("@/components/Footer"));
 import Hero from "@/components/Hero";
 const RecentProjects = dynamic(() => import("@/components/RecentProjects"), {
   ssr: false,
@@ -10,7 +10,7 @@ import dynamic from "next/dynamic";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
