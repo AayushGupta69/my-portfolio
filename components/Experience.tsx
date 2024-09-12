bd0c77225a90697f177e122683f16f755f79c6fd
Aayush Gupta <72346589+AayushGupta69@users.noreply.github.com>
2024-09-12 20:36:14 +0530

    Navbar fix (#10)
    * Made the floating navbar mobile responsive
    
    * Updated data and id for sections

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Experience = () => {
   return (
-    <section id="work-experience" className="py-20 w-full">
+    <section id="experience" className="py-20 w-full">
       <h1 className="heading">
         My <span className="text-purple">work experience</span>
       </h1>
